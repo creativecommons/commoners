d3ce41b17a4dae332c201edea2ae1726c62afe2a
Rob Myers <rob@robmyers.org>
2017-09-22 20:18:28 -0700

    Make static front page full width in theme.

---
+++ themes/cc-commoners/full-width.php
@@ -0,0 +1,30 @@
+<?php /* Template Name: Full Width */
+
+get_header();
+
+?>
+
+waah
+
+<div class="wrap page-full-width">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
