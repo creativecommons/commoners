f90d5a5417274d9033cba9b592a822ce0c2c73c4
hugosolar <hugo.solar@gmail.com>
2018-11-08 16:46:19 -0300

    admin script version to avoid browser caché

--- plugins/cc-global-network/cc-global-network.php
+++ plugins/cc-global-network/cc-global-network.php
@@ -352,6 +352,7 @@ add_action('admin_enqueue_scripts', 'ccgn_admin_enqueue_scripts');
 function ccgn_admin_enqueue_scripts($hook_suffix) {
     global $pagenow;
     $style_version = '2.0.1';
+    $script_version = '2.0.1';
     if (is_admin() && strstr($hook_suffix, 'global-network') ) {
         wp_enqueue_style('datatables-style', CCGN_URL_PATH . 'admin/assets/css/datatables.css');
         wp_enqueue_style('admin-style', CCGN_URL_PATH . 'admin/assets/css/admin_styles.css',array(),$style_version);
@@ -359,7 +360,7 @@ function ccgn_admin_enqueue_scripts($hook_suffix) {
         wp_enqueue_script('jquery');
         wp_enqueue_script('jquery-ui-datepicker');
         wp_enqueue_script('datatables', CCGN_URL_PATH . 'admin/assets/js/datatables.min.js', array('jquery'), '', '');
-        wp_enqueue_script('ccgn-script', CCGN_URL_PATH . 'admin/assets/js/script.js', array('jquery'), '', '');
+        wp_enqueue_script('ccgn-script', CCGN_URL_PATH . 'admin/assets/js/script.js', array('jquery'), $script_version, '');
         wp_localize_script('ccgn-script', 'wpApiSettings', array(
             'root' => esc_url_raw(rest_url()),
             'nonce' => wp_create_nonce('wp_rest'),
