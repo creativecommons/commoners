3bad86ceaaeaa2dc55781e0f24e628a0a931e5d0
hugosolar <hugo.solar@gmail.com>
2018-11-14 08:06:40 -0300

    condition added so it can't be voted someone who isn't vouched

--- plugins/cc-global-network/admin/user-application-page.php
+++ plugins/cc-global-network/admin/user-application-page.php
@@ -436,8 +436,10 @@ function ccgn_application_users_page_render_state ( $applicant_id, $state ) {
         ccgn_application_users_page_pre_form ( $applicant_id );
     } elseif ( $state == CCGN_APPLICATION_STATE_VOUCHING ) {
         if (ccgn_current_user_is_final_approver() || ccgn_current_user_is_membership_council()) {
-            echo _('<h3>Vote</h3>');
-            ccgn_application_users_page_vote_form ( $applicant_id );
+            if ( ccgn_application_can_be_voted( $applicant_id ) ) {
+                echo _('<h3>Vote</h3>');
+                ccgn_application_users_page_vote_form ( $applicant_id );
+            }
             if (ccgn_current_user_is_final_approver()) {
                 echo _('<h3>Final Approval</h3>');
                 ccgn_application_users_page_final_approval_form( $applicant_id );
