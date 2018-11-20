40992c0eaacd05d6aa2cc4c30c7167359791d566
hugosolar <hugo.solar@gmail.com>
2018-11-20 13:45:31 -0300

    template page for standard content

---
+++ themes/cc-commoners/template-content.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Template Name: Content Page
+ */
+
+get_header(); ?>
+
+
+	<div class="block-area">
+
+		<div class="inner-section-list entry-content">
+
+          <?php get_template_part('template-parts/loop/frontpage-loop'); ?>
+
+		</div>
+
+	</div><!--/.block-area-->
+
+<?php get_footer(); ?>
