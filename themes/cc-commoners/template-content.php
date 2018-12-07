69115d687a67d35d69a75abea37beb4648e3bb44
hugosolar <hugo.solar@gmail.com>
2018-12-07 15:47:09 -0300

    adding  a new template page for logged in users

--- themes/cc-commoners/template-content.php
+++ themes/cc-commoners/template-content.php
@@ -6,18 +6,19 @@ Template Name: Content Page
 
 get_header(); 
 ?>
+<div class="cc-content-area">
+    <div class="inner-section-title">
+        <h1><?php the_title(); ?></h1>
+    </div>
+    <div class="block-area">
 
-<div class="inner-section-title">
-    <h1><?php the_title(); ?></h1>
+        <div class="inner-section-list entry-content">
+
+            <?php get_template_part('template-parts/loop/frontpage-loop'); ?>
+
+        </div>
+
+    </div><!--/.block-area-->
 </div>
-	<div class="block-area">
-
-		<div class="inner-section-list entry-content">
-
-          <?php get_template_part('template-parts/loop/frontpage-loop'); ?>
-
-		</div>
-
-	</div><!--/.block-area-->
 
 <?php get_footer(); ?>
---
+++ themes/cc-commoners/template-logged.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Template Name: Logged in content Page
+ */
+
+get_header();
+?>
+<div class="cc-content-area">
+    <?php if (is_user_logged_in()): ?>
+    <div class="inner-section-title">
+        <h1><?php the_title(); ?></h1>
+    </div>
+    <?php endif; ?>
+    <div class="block-area">
+        <?php $logged_class = (is_user_logged_in()) ? 'login' : 'not-login'; ?>
+        <div class="inner-section-list entry-content <?php echo $logged_class ?>">
+            <?php if (is_user_logged_in()) : ?>
+                <?php get_template_part('template-parts/loop/frontpage-loop'); ?>
+            <?php else: ?>
+                <div class="callout warning">
+                    <h5>Sorry</h5>
+                    <p>You have to be logged in to see this content</p>
+                </div>
+            <?php endif; ?>
+        </div>
+
+    </div><!--/.block-area-->
+</div>
+
+<?php get_footer(); ?>
