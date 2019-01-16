5fc6ca2316209e2088db45c3386a1f8fbcaca157
hugosolar <hugo.solar@gmail.com>
2019-01-16 17:21:03 -0300

    home refactoring

--- themes/cc-commoners/footer.php
+++ themes/cc-commoners/footer.php
@@ -3,43 +3,36 @@
 </div><!-- .page-body -->
 
 
-<footer>
+<footer class="main-footer">
+    <div class="footer-content">
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x align-justify">
+            <div class="large-4 cell">
+                <a href="/" class="logo"><span>CC Network</span></a>
+            </div>
+            <div class="large-5 cell">
+                <div class="footer-text-block">
+                    <div class="license-icons">
+                        <a rel="license" href="https://creativecommons.org/licenses/by/4.0/" title="Creative Commons Attribution 4.0 International license">
+                        <i class="cc-icon-cc"></i>
+                        <i class="cc-icon-cc-by"></i>
+                        </a>
+                    </div>
 
+                    <p>Except where otherwise <a href="https://creativecommons.org/policies#license">noted</a>, content on this site is licensed under a <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International license</a>. <a href="https://creativecommons.org/website-icons">Icons</a> by The Noun Project.</p>
 
-  <div class="footer-content">
-
-
-    <a href="/" class="logo"><span>CC Network</span></a>
-
-
-    <div class="footer-text-block">
-
-      <div class="license-icons">
-        <a rel="license" href="https://creativecommons.org/licenses/by/4.0/" title="Creative Commons Attribution 4.0 International license">
-          <i class="cc-icon-cc"></i>
-          <i class="cc-icon-cc-by"></i>
-        </a>
-      </div>
-
-      <p>Except where otherwise <a href="https://creativecommons.org/policies#license">noted</a>, content on this site is licensed under a <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International license</a>. <a href="https://creativecommons.org/website-icons">Icons</a> by The Noun Project.</p>
-
-      <nav class="bottom-nav">
-        <?php wp_nav_menu( array(
-              'depth'=>1,
-              'after'=>'<span class="sep">|</span>',
-              'theme_location' => 'bottom',
-              'menu_id'        => 'bottom-menu',
-        ) ); ?>
-      </nav>
-      
-      <?php /*if ( is_active_sidebar( 'sidebar-footer-text' ) ) {
-          dynamic_sidebar( 'sidebar-footer-text' );
-      }*/ ?>
-
-    </div><!--/.footer-text-block-->
-
-  </div>
-  
+                    <nav class="bottom-nav">
+                        <?php wp_nav_menu(array(
+                            'depth' => 1,
+                            'theme_location' => 'bottom',
+                            'menu_id' => 'bottom-menu',
+                            'items_wrap' => '<ul id = "%1$s" class = "menu align-right %2$s">%3$s</ul>'
+                        )); ?>
+                    </nav>
+                </div><!--/.footer-text-block-->
+            </div>
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
